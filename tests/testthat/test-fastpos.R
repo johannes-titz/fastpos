7346c9c9fcf50f2a6e182c3550c79aa56104db50
Johannes Titz <johannes@titz.science>
2020-01-08 16:26:19 +0100

    add test for relative precision and unloading the package

--- tests/testthat/test-fastpos.R
+++ tests/testthat/test-fastpos.R
@@ -1,6 +1,8 @@
 set.seed(20191204)
 cpos <- find_critical_pos(rho = c(.1, .7), sample_size_max = 1000,
                           n_studies = 10000)
+sim <- find_critical_pos(rho = .5, precision_rel = T)
+
 cpos_old <- read.csv("cpos.csv")
 test_that("previous values can be reproduced", {
   expect_equal(cpos, cpos_old, check.attributes = F)
@@ -17,3 +19,10 @@ test_that("Schoenbrodt and Perugini's values are close to fastpos' values for
   # individual relative deviation within 2%
   expect_true(all(abs(diff_rel) < .02))
 })
+
+test_that("unloading package works",
+          expect_null(detach(package:fastpos, unload = T)))
+
+
+test_that("relative precision works",
+          expect_equal(c(sim$lower_limit, sim$upper_limit), c(.45, .55)))
