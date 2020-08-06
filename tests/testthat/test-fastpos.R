5ee31525365a3b28aa0331611609c0261b9ac7fe
Johannes Titz <johannes@titz.science>
2020-08-06 12:48:44 +0200

    add test for replace parameter

--- tests/testthat/test-fastpos.R
+++ tests/testthat/test-fastpos.R
@@ -36,6 +36,20 @@ test_that("Schoenbrodt and Perugini's values are close to fastpos' values for
   expect_true(all(abs(diff_rel_mc) < .02))
 })
 
+cpos_mc_replace <- find_critical_pos(rho = c(.1, .7), sample_size_max = 1000,
+                             n_studies = 40000,
+                             n_cores = future::availableCores(), replace = FALSE)
+
+diff_rel_mc_replace <- (nicebread-cpos_mc[,2:4])/nicebread
+
+test_that("Schoenbrodt and Perugini's values are close to fastpos' values for
+          rho = .1 and .7 (with multiple cores) and replace = TRUE", {
+  # average relative deviation within 1%
+  expect_true(abs(mean(unlist(diff_rel_mc_replace))) < 0.01)
+  # individual relative deviation within 2%
+  expect_true(all(abs(diff_rel_mc_replace) < .02))
+})
+
 test_that("unloading package works",
           expect_null(detach(package:fastpos, unload = T)))
 
