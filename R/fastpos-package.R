9a81ac970bb94d544b4c65c228a7b7a2acd008ba
Johannes Titz <johannes@titz.science>
2022-07-26 15:17:02 +0200

    add extra file for package docu

---
+++ R/fastpos-package.R
@@ -0,0 +1,22 @@
+#' @details
+#' In most cases you will just need the function [fastpos::find_critical_pos()]
+#' which will you give you the critical point of stability for your specific
+#' parameters. If you are interested in more complicated analysis you might want
+#' to look at the function [fastpos::simulate_pos()], which is a C++
+#' functions to calculate correlations and return points of stability.
+#'
+#' @references Schönbrodt, F. D. & Perugini, M. (2013). At what sample size do
+#' correlations stabilize? *Journal of Research in Personality, 47*,
+#' 609-612. [https://doi.org/10.1016/j.jrp.2013.05.009](https://doi.org/10.1016/j.jrp.2013.05.009)
+#'
+#' Schönbrodt, F. D. & Perugini, M. (2018) Corrigendum to “At what sample size
+#' do correlations stabilize?” \[J. Res. Pers. 47 (2013) 609–612.
+#' https://doi.org/10.1016/j.jrp.2013.05.009\]. \emph{Journal of Research in
+#' Personality, 74}, 194. [https://doi.org/10.1016/j.jrp.2018.02.010](https://doi.org/10.1016/j.jrp.2018.02.010)
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom lifecycle deprecated
+## usethis namespace: end
+NULL
