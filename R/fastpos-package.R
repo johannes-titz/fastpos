91c3b422d78fd0ca013585c461b527fe6ec13068
johannes-titz <johannes@titz.science>
2022-07-28 09:45:58 +0200

    use correct doi format

--- R/fastpos-package.R
+++ R/fastpos-package.R
@@ -7,12 +7,12 @@
 #'
 #' @references Schönbrodt, F. D. & Perugini, M. (2013). At what sample size do
 #' correlations stabilize? *Journal of Research in Personality, 47*,
-#' 609-612. [https://doi.org/10.1016/j.jrp.2013.05.009](https://doi.org/10.1016/j.jrp.2013.05.009)
+#' 609-612. \doi{10.1016/j.jrp.2013.05.009}
 #'
 #' Schönbrodt, F. D. & Perugini, M. (2018) Corrigendum to “At what sample size
 #' do correlations stabilize?” \[J. Res. Pers. 47 (2013) 609–612.
 #' https://doi.org/10.1016/j.jrp.2013.05.009\]. \emph{Journal of Research in
-#' Personality, 74}, 194. [https://doi.org/10.1016/j.jrp.2018.02.010](https://doi.org/10.1016/j.jrp.2018.02.010)
+#' Personality, 74}, 194. \doi{10.1016/j.jrp.2018.02.010}
 #' @keywords internal
 "_PACKAGE"
 
